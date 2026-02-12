b37fb00fd76557d7e43af677b32a42d3e98a1bb3
Chormiak <william.chormi@gmail.com>
2026-02-12 15:59:25 -0300

    FEAT: Basic configuration created and added core files (fonts, icon)

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,72 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2025-07-15",
+  devtools: { enabled: true },
+  css: ["~/assets/css/main.css"],
+  app: {
+    head: {
+      title: "William Chormiak – Desenvolvedor Backend Node.js & Django",
+
+      meta: [
+        { charset: "utf-8" },
+
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+
+        {
+          name: "description",
+          content:
+            "Desenvolvedor Frontend especializado em Vue, Nuxt e performance web. Projetos reais com foco em arquitetura limpa e boas práticas.",
+        },
+
+        // // Open Graph
+        // {
+        //   property: "og:title",
+        //   content: "Seu Nome – Desenvolvedor Frontend",
+        // },
+        // {
+        //   property: "og:description",
+        //   content:
+        //     "Projetos em Vue e Nuxt com foco em performance, SEO e arquitetura moderna.",
+        // },
+        // {
+        //   property: "og:type",
+        //   content: "website",
+        // },
+        // {
+        //   property: "og:url",
+        //   content: "https://seusite.com",
+        // },
+        // {
+        //   property: "og:image",
+        //   content: "https://seusite.com/og-image.png",
+        // },
+
+        // // Twitter
+        // {
+        //   name: "twitter:card",
+        //   content: "summary_large_image",
+        // },
+        // {
+        //   name: "twitter:title",
+        //   content: "Seu Nome – Desenvolvedor Frontend",
+        // },
+        // {
+        //   name: "twitter:description",
+        //   content: "Portfólio com projetos em Vue, Nuxt e foco em performance.",
+        // },
+        // {
+        //   name: "twitter:image",
+        //   content: "https://seusite.com/og-image.png",
+        // },
+      ],
+
+      link: [
+        {
+          rel: "icon",
+          type: "image/svg+xml",
+          href: "/favicon.svg",
+        },
+      ],
+    },
+  },
+});
