183200d149d764c4c37cf4bc3959546b1a32ab77
Chormiak <william.chormi@gmail.com>
2026-02-16 18:52:03 -0300

    FIX: Updating Class Name in CSS

--- app/error.vue
+++ app/error.vue
@@ -8,7 +8,7 @@
   </NuxtLayout>
 </template>
 <style scoped>
-.box .terminal section {
+.box .content section {
   display: flex;
   width: 100%;
   height: 100%;
@@ -17,12 +17,12 @@
   justify-content: center;
 }
 
-.box .terminal section a {
+.box .content section a {
   margin-top: 1.25rem;
   background-color: var(--main-general-color-clear);
   color: var(--main-background-color);
 }
-.app.green .box .terminal section a {
+.app.green .box .content section a {
   background-color: var(--main-general-color-green);
 }
 
