ddc327c8b628a0b0f98f9a06d16a8e600c29d1ec
Chormiak <william.chormi@gmail.com>
2026-02-23 14:39:53 -0300

    CI: change name of reset.css and icon/

---
+++ app/components/icons/Error.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="icon">
+    <p>404</p>
+    <pre class="error">
+        :::      :::::::      ::: 
+      :+:      :+:   :+:    :+:   
+    +:+ +:+   +:+   +:+   +:+ +:+ 
+  +#+  +:+   +#+   +:+  +#+  +:+  
++#+#+#+#+#+ +#+   +#+ +#+#+#+#+#+ 
+      #+#   #+#   #+#       #+#    
+    ###    #######        ###     
+    </pre>
+  </div>
+</template>
+<style scoped>
+p {
+  display: none;
+}
+@media (max-width: 610px) {
+  .icon .error {
+    display: none;
+  }
+  p {
+    display: block;
+    font-weight: bold;
+  }
+}
+</style>
---
+++ app/components/icons/Name.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="icon">
+    <pre class="name">
+ _       ___ _____               
+| |     / (_) / (_)___ _____ ___ 
+| | /| / / / / / / __ `/ __ `__ \
+| |/ |/ / / / / / /_/ / / / / / /
+|__/|__/_/_/_/_/\__,_/_/ /_/ /_/ 
+      </pre
+    >
+    <pre class="lastname">
+     ________                         _       __  
+    / ____/ /_  ____  _________ ___  (_)___ _/ /__
+   / /   / __ \/ __ \/ ___/ __ `__ \/ / __ `/ //_/
+  / /___/ / / / /_/ / /  / / / / / / / /_/ / ,<   
+  \____/_/ /_/\____/_/  /_/ /_/ /_/_/\__,_/_/|_|  
+      </pre
+    >
+  </div>
+  
+</template>
+<style scoped>
+.icon {
+  display: flex;
+}
+
+@media (max-width: 1300px) {
+  .icon .lastname {
+    display: none;
+  }
+}
+@media (max-width: 900px) {
+  .icon .lastname {
+    display: block;
+  }
+}
+@media (max-width: 800px) {
+  .icon .lastname {
+    display: none;
+  }
+}
+@media (max-width: 350px) {
+  .icon .name {
+    display: none;
+  }
+}
+
+</style>
