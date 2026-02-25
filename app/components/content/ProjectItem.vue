c00447839633b3271be81e1c09f821418ad9efe7
Chormiak <william.chormi@gmail.com>
2026-02-25 19:39:53 -0300

    FEAT: Creating project list with components

---
+++ app/components/content/ProjectItem.vue
@@ -0,0 +1,60 @@
+<script setup lang="ts">
+defineProps<{
+  name: string;
+  problem: string;
+  stack: string[];
+  architecture: string[];
+  features: string[];
+  status: string;
+  repo: `https://github.com/${string}`;
+  details: `/${string}`;
+}>();
+</script>
+<template>
+  <ul>
+    <li>[{{ name }}]</li>
+    <li>problem: {{ problem }}</li>
+    <li>stack: {{ stack.join(", ") }}</li>
+    <li>architecture: {{ architecture.join(" + ") }}</li>
+    <li>
+      features:
+      <ul class="features">
+        <li v-for="feature in features" :key="name + '/' + feature">
+          {{ feature }}
+        </li>
+      </ul>
+    </li>
+    <li>status: {{ status }}</li>
+    <li>
+      repo: <a :href="repo">{{ repo }}</a>
+    </li>
+    <li>
+      details: <NuxtLink :to="details">{{ details }}</NuxtLink>
+    </li>
+  </ul>
+</template>
+<style scoped>
+
+@media (min-width: 901px) {
+  .features {
+    list-style-position: inside;
+  }
+
+  .features li::marker {
+    content: "- ";
+  }
+}
+
+@media (max-width: 900px) {
+  .features {
+    display: inline;
+  }
+  .features li {
+    display: inline;
+  }
+
+  .features li + li::before {
+    content: ", ";
+  }
+}
+</style>
---
+++ app/components/content/ProjectList.vue
@@ -0,0 +1,45 @@
+<template>
+  <ol>
+    <li>
+      <ContentProjectItem
+        repo="https://github.com/Chormiak/portfolio"
+        problem="Frase"
+        details="/projects/cashflow-api"
+        name="Nome"
+        :stack="['Typescript', 'Nodejs', 'Fastify']"
+        :architecture="['layered (controller/service/repository)']"
+        :features="['auth(jwt)', 'hashing']"
+        status="Em desenvolvimento"
+      />
+    </li>
+    <li>
+      <ContentProjectItem
+        repo="https://github.com/Chormiak/portfolio"
+        problem="Frase"
+        details="/projects/cashflow-api"
+        name="Nome"
+        :stack="['Typescript', 'Nodejs', 'Fastify']"
+        :architecture="['layered (controller/service/repository)']"
+        :features="['auth(jwt)', 'hashing']"
+        status="Em desenvolvimento"
+      />
+    </li>
+  </ol>
+</template>
+<style scoped>
+ol {
+  counter-reset: item;
+  display: flex;
+  flex-direction: column;
+  gap: 1.25rem;
+}
+ol li {
+  display: flex;
+  gap: 0.65rem;
+  counter-increment: item;
+}
+
+ol li::before {
+  content: "[" counter(item) "]";
+}
+</style>
