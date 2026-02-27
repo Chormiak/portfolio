95d63f087df7fa6f000959dcc8cc0fe0db01b876
Chormiak <william.chormi@gmail.com>
2026-02-27 13:37:21 -0300

    FEAT: Created list on professional experience

---
+++ app/components/content/Work/Item.vue
@@ -0,0 +1,38 @@
+<template>
+  <article>
+    <h4>{{ 2025 }} - Backend Developer</h4>
+    <div>
+      <p><stroke>Enterprise:</stroke> IFC</p>
+      <p><stroke>Foco:</stroke> APIs REST, arquitetura em camadas</p>
+      <p><stroke>Conceitos:</stroke> HTTP, autenticação, hashing, JWT</p>
+    </div>
+  </article>
+</template>
+<style scoped>
+p {
+  padding-left: 1ch;
+  text-indent: -1ch;
+}
+
+article {
+  position: relative;
+  margin-left: 0.65rem;
+}
+
+article h4::before {
+  content: "*";
+  background-color: var(--main-background-color);
+  left: calc(-0.65rem - 0.0625rem);
+  transform: translateX(-50%);
+  position: absolute;
+}
+article div {
+  margin-left: 1.25rem;
+}
+
+@media (max-width: 900px) {
+  article div {
+    margin-left: 0.65rem;
+  }
+}
+</style>
---
+++ app/components/content/Work/List.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul>
+    <li v-for="i in 3">
+      <ContentWorkItem />
+    </li>
+  </ul>
+</template>
+<style scoped>
+ul {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  padding-left: 0.125rem;
+  row-gap: 3.75rem;
+  margin-left: 1.25rem;
+}
+ul::before {
+  content: "";
+  position: absolute;
+  left: 0;
+  top: 2.5px;
+  bottom: 0;
+  width: 0.125rem;
+
+  background: repeating-linear-gradient(
+    to bottom,
+    var(--main-general-color-clear) 0,
+    var(--main-general-color-clear) 0.95rem,
+    transparent 0.95rem,
+    transparent 1.25rem
+  );
+}
+
+@media (max-width: 900px) {
+  ul {
+    margin-left: 0.65rem;
+    row-gap: 2.5rem;
+  }
+}
+</style>
